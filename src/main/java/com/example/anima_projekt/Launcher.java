b3dcef7e328904b231c03ef8e0b6d378179d979d
ErikB15 <erik.k.braun@gmail.com>
2026-04-01 17:09:22 +0200

    la till hejsan

--- src/main/java/com/example/anima_projekt/Launcher.java
+++ src/main/java/com/example/anima_projekt/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println("hejsan");
         Application.launch(HelloApplication.class, args);
     }
 }
