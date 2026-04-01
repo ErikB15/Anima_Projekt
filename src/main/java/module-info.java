4157de012ab5dbaa46731d0fad7bdec5783d6602
ErikB15 <erik.k.braun@gmail.com>
2026-04-01 17:04:17 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.anima_projekt {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.anima_projekt to javafx.fxml;
+    exports com.example.anima_projekt;
+}
