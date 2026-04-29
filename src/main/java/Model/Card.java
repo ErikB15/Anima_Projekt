2379d9541b9504514db7a942ec2dbaa207637ecf
Jim Ström <Jimbokalla@gmail.com>
2026-04-29 16:30:59 +0200

    Gjort klart basen för Board,Card och Player
    Även kommenterat på nästan alla metoderna, gjort klart logiken för player och Board samt fixat småsaker inne i Card klassen. Lagt till en "Test" formel hittils.

--- src/main/java/Model/Card.java
+++ src/main/java/Model/Card.java
@@ -12,6 +12,19 @@ public class Card {
     private boolean hasAttackedThisTurn;
     private String imagePath;
 
+
+    /**
+     * Denna klassen är egentligen typ bara sparande av information.
+     * Har lite hantering också men den är inte mycket.
+     * Har gjort en TEMPORÄR  formel som kan komma att ändras, gjort för testning.
+     * @param cardName - Namnet på kortet.
+     * @param cardAD - Attack förmågan av kortet.
+     * @param cardMaxHP - Kortets max HP.
+     * @param cardID - Kortets ID.
+     * @param cardEffect - En effekt som kortet eventuellt kommer at ha.
+     * @param imagePath - Vägen till bilden som är kopplad till kortet.
+     * @author Jim Ström
+     */
     public Card(String cardName, int cardAD, int cardMaxHP, int cardID, Effect cardEffect, String imagePath){
         this.cardAD = cardAD;
         this.cardMaxHP = cardMaxHP;
@@ -19,8 +32,42 @@ public class Card {
         this.cardName = cardName;
         this.cardEffect = cardEffect;
         this.imagePath = imagePath;
-        // Här ska det läggas till en formel för att räkna ut kortets kostnad
+        this.cardCurrentHP = cardMaxHP;
+
+        cardCost = (Math.round((float)(cardAD + cardMaxHP) / 4));
+        // Temporär formel som lagts till endast för testning.
     }
+
+    /**
+     * Gjord för att kortet ska kunna ta skada.
+     * Om skadan sänker kortets hp under 0 så blir hp:et istället bara 0.
+     * Annars tar den skadan som vanligt.
+     * @param damage - Skadan som sker på kortet.
+     * @author Jim Ström.
+     */
+    public void takeDamage(int damage) {
+        if(cardCurrentHP - damage <= 0){
+            cardCurrentHP = 0;
+        }else{
+            cardCurrentHP -= damage;
+        }
+    }
+
+    /**
+     * Denna kollar bara om kortet är dött eller inte, gjort nu, kanske inte används, för se.
+     * @return Returnerar en boolean som säger sant om kortet är dött eller falskt om kortet inte är dött.
+     * @author Jim Ström
+     */
+    public boolean isDead() {return cardCurrentHP <= 0;}
+
+
+    /**
+     * Normaltvis försöker jag göra en kommentar på allting.
+     * Men om ni skrollar ner lite, tror jag ni förstår varför jag inte gör en kommentar på allting under.
+     * Kommer det in att vi måste göra kommentarer på allt? Då får jag göra det men i nuläget skippar vi detta.
+     * Massa getters och setters för alla de olika variablerna som Card klassen innehåller.
+     * @author Jim Ström
+     */
 
 
     public void setCardCurrentHP(int cardCurrentHP) {this.cardCurrentHP = cardCurrentHP;}
@@ -46,4 +93,10 @@ public class Card {
     }
 
     public int getCardCurrentHP() {return cardCurrentHP;}
+
+    public boolean getAsleep(){return asleep;}
+
+    public boolean getHasAttackedThisTurn(){return hasAttackedThisTurn;}
+
+    public int getCardCost() {return cardCost;}
 }
--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -15,6 +15,7 @@ public class Player {
      * Deck - De korten som spelaren drar från varje runda.
      * Hand - De korten en spelare kan spela och har i sin hand varje runda.
      * Graveyard - De korten en spelare har förlorat.
+     * @author Jim Ström
      */
     public Player(){
         this.hp = 30; // PS. 30 Är bara test "value" just nu.
@@ -26,6 +27,7 @@ public class Player {
     /**
      * Lägger bara till ett kort, nödvändigt för första fasen där de två spelarna väljer kort.
      * @param addedCard Kortet en spelare valt att de vill ha.
+     * @author Jim Ström
      */
     public void addCardToDeck(Card addedCard){
         deck.add(addedCard);
@@ -36,11 +38,10 @@ public class Player {
      * Den har några safety checks, är spelarens kortlek tom så reshuffle vi den genom en metod.
      * Sen kollar vi igen, detta är en safety check för att se till att allt funkade som det skulle.
      * Sedan drar vi kortet från kortleken.
-     * @param nbrOfCardsToDraw - Hur många kort som ska dras, ändras beroende på antalet rundor.
+     * @author Jim Ström
      */
-    public void drawCard(int nbrOfCardsToDraw) {
-        for (int i = 0; i < nbrOfCardsToDraw; i++) {
-
+    public void fillHandToThree() {
+        while (hand.size() < 3) {
             if (deck.isEmpty()) {
                 reshuffleDeck();
             }
@@ -57,8 +58,12 @@ public class Player {
      * Det här är för när ett kort dör, då ska det läggas in i "graveyard" kortleken.
      * Notera att vi nollställer kortets stats efter vi slängt in den i graveyard kortleken.
      * @param deadCard - Det kortet som blev dödat.
+     * @author Jim Ström
      */
     public void sendCardToGraveyard(Card deadCard){
+        if (deadCard == null) {
+            return;
+        }
         graveyard.add(deadCard);
         deadCard.setCardCurrentHP(deadCard.getCardMaxHP());
         deadCard.setAsleep(false);
@@ -69,6 +74,7 @@ public class Player {
      * Simpelt, lägger till alla korten från graveyard in i din riktiga deck.
      * Sedan clearar vi graveyard och gör den tom igen.
      * Därefter shufflar vi den genom en "Collection" metod.
+     * @author Jim Ström
      */
     public void reshuffleDeck(){
         deck.addAll(graveyard);
@@ -77,24 +83,76 @@ public class Player {
     }
 
     /**
-     * Kan du gissa vad detta göra?
-     * @param hp - HP:et du vill sätta det till.
-     * @return - En boolean, syftet med denna är att göra en enkel check som till och med kollar ifall matchen
-     * är över.
+     * Checkar först så att index är i handen, borde göras innan men alltid bra att vara säker.
+     * Stämmer indexen på något sätt inte så returneras null, lär behöva hanteras i controllern.
+     * @param index - Indexet på kortet i handen som ska tas bort.
+     * @return - Ger kortet som ska tas bort (för att kunna lägga det på planen)
+     * @author Jim Ström
      */
-    public boolean setHp(int hp) {
-        this.hp = hp;
-        if(hp <= 0){
-            return true;
+    public Card removeCardFromHand(int index){
+        if(index < 0 || index >= hand.size()){
+            return null;
         }
-        return false;
+        return hand.remove(index);
+    }
+
+    /**
+     * En if check för att sätta HP:et till 0 om det skulle egentligen bli mindre än 0.
+     * För att förenkla och inte göra det problematiskt med andra int checks.
+     * @param damage - Skadan gjord på spelaren.
+     * @author Jim Ström
+     */
+    public void takeDamage(int damage) {
+        if((hp - damage) <= 0){
+            this.hp = 0;
+        }else {
+            this.hp = (hp - damage);
+        }
+    }
+
+    /**
+     * Kollar om spelaren är död, är HP:et mindre eller lika med noll (borde inte kunna bli mindre),
+     * så returneras true, då spelaren är död.
+     * @return En boolean som säger ja eller nej beroende på om spelaren är död eller inte.
+     * @author Jim Ström.
+     */
+    public boolean isDead(){
+        return hp <= 0;
     }
 
     /**
      * Enkel getter.
      * @return Ger tillbaka HP:et av spelaren
+     * @author Jim Ström
      */
     public int getHp() {
         return hp;
+    }
+
+    /**
+     * Enkel getter för att hämta handen spelaren har.
+     * @return En arraylist av cards som är spelarens hand.
+     * @author Jim Ström
+     */
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    /**
+     * Enkel getter för att hämta kortlek spelaren har.
+     * @return En arraylist av cards som är spelarens kortlek.
+     * @author Jim Ström
+     */
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    /**
+     * Enkel getter för att hämta graveyard spelaren har.
+     * @return En arraylist av cards som är spelarens graveyard.
+     * @author Jim Ström
+     */
+    public ArrayList<Card> getGraveyard() {
+        return graveyard;
     }
 }
